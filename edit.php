630b98fafa3b74c647cc681cdb2fedded912de5e
Nmurphy0507 <Nathanmurphy0507@gmail.com>
2024-08-26 10:38:22 -0400

    Some Small changes

--- edit.php
+++ edit.php
@@ -58,7 +58,7 @@
     <title>PHP Keysorting App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="./src/homepage.css">
+    <link rel="stylesheet" href="./src/edit.css">
 </head>
 
 <body>
