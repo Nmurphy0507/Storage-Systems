060cc2d41dfda1261a179c5be0de4316572da77d
Nmurphy0507 <Nathanmurphy0507@gmail.com>
2025-04-03 18:55:50 -0400

    made an events calender section in the homepage

--- home.php
+++ home.php
@@ -96,7 +96,7 @@ while ($row_groups = mysqli_fetch_assoc($result_groups)) {
 table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
-  width: 85%;
+  width: 75%;
   margin: auto;
 }
 
@@ -106,17 +106,38 @@ td, th {
   padding: 10px;
 }
 
-th{
-    background-color: rgb(40, 40, 40);
-    color: white;
+th {
+  background-color: rgb(40, 40, 40);
+  color: white;
 }
 
 tr {
   background-color:rgb(255, 255, 255);
 }
 
-h2{
-    text-align: center;
+h2 {
+  text-align: center;
+  color: black;
+}
+
+.upcoming-events {
+  background-color: white;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.buildings-table {
+
+}
+
+.groups-table {
+  
+}
+
+.calender-buttons {
+  text-align: right;
+  padding: auto;
+  margin: auto;
 }
 </style>
 </head>
@@ -152,20 +173,32 @@ h2{
 </div>
 
 <div class="row">
-
     <div class="column" style="overflow-x:auto;">
-        <h2>Total Number of Keys Out - <?= isset($total_checked_in) ? $total_checked_in : 0; ?></h2>
-        <h2> Upcoming Camp Calender</h2>
+      <div class="upcoming-events">
+        <h2>Upcoming Events Calender</h2>
+          <table style="width: 100%;">
+            <tr>
+              <th> Upcoming Events Calender </th>
+              <td>FSY Week 1 10/25/3024</td>
+              <td>FSY Week 2 12/24/3423</td>
+            </tr>
+          </table>
+        <div class="calender-buttons">
+          <button class="btn btn-secondary my-2 mx-2">Save</button>
+          <button class="btn btn-secondary my-2 mx-2">Edit</button>
+          <button class="btn btn-secondary my-2 mx-2">Delete</button>
+        </div>
+      </div>
     </div>
 
     <div class="column" style="overflow-x:auto;">
-        <h2>Building Residence</h2>
+      <div class="buildings-table">
+        <h2>Total Building Residence - <?= isset($total_checked_in) ? $total_checked_in : 0; ?></h2>
         <table>
             <tr>
                 <th>Building</th>
                 <th>Checked in Residence</th>
             </tr>
-            <!-- Display for each building -->
             <tr>
                 <td>Allen</td>
                 <td><?= isset($building_counts['Allen']) ? $building_counts['Allen'] : 0; ?></td>
@@ -208,9 +241,11 @@ h2{
             </tr>
         </table>
       </div>
+    </div>
 
-    <div class="column" style="overflow-x:auto;">
-        <h2>Groups</h2>
+  <div class="column" style="overflow-x:auto;">
+    <div class="groups-table">
+    <h2>Total Residence Per Group</h2>
         <table>
           <tr>
             <th>Groups</th>
@@ -277,12 +312,12 @@ h2{
             <td><?= isset($group_counts['Other']) ? $group_counts['Other'] : 0; ?></td>
           </tr>
         </table>
+      </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybU5p2Uj1d1iz1pSA9Wdnlrp9bJhFsD7/TfZp7xYfuN0KSf4I" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-    <!-- Custom JS -->
     <script src=""></script>
 
     <div class="footer">
